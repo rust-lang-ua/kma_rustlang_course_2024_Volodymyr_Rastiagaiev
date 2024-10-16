d8a76a6291c3dc5d629a37cc825df1b7534ed363
zxccxccxz <galexoks@gmail.com>
2024-10-17 00:27:06 +0300

    6

--- rustlings/exercises/error_handling/errors4.rs
+++ rustlings/exercises/error_handling/errors4.rs
@@ -1,7 +1,7 @@
 // errors4.rs
 // Make this test pass! Execute `rustlings hint errors4` for hints :)
 
-// I AM NOT DONE
+use std::convert::TryInto;
 
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
@@ -14,7 +14,11 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        Ok(PositiveNonzeroInteger(value as u64))
+        let res: u64 = value.try_into().map_err(|_| {CreationError::Negative})?;
+        if res == 0 {
+            return Err(CreationError::Zero);
+        }
+        Ok(PositiveNonzeroInteger(res))
     }
 }
 
